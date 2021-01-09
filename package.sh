b0852099aeb5628aa553e6dc62b8f98b338380ce
Leonel M <leonellobato21@gmail.com>
2021-01-08 18:48:05 -0700

    Azathot V.1.3.5

--- package.sh
+++ package.sh
@@ -1,6 +1,5 @@
 apt update && apt upgrade -y
-apt install python -y
-apt install w3m -y
+apt install python w3m -y
 pip install -r tools/doc/requirements.txt
 mv tools/azathot/azathot.py .
 chmod +x azathot.py
