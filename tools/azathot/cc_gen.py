33d0fbd55a9be5a8515eacab8b834ad8f5e467e4
Leonel M <leonellobato21@gmail.com>
2021-01-18 20:47:27 -0700

    Azathot v.1.3.5

--- tools/azathot/cc_gen.py
+++ tools/azathot/cc_gen.py
@@ -4,27 +4,17 @@ from huepy import *
 
 __version__ = "1.3.6"
 
-#ALGORITHM LUHN
-def CHECKCARD(card_number):
-    sum = 0
-    num_digits = len(card_number)
-    oddeven = num_digits & 1
-
-    for count in range(0, num_digits):
-        digit = int(card_number[count])
-
-        if not (( count & 1 ) ^ oddeven ):
-            digit = digit * 2
-        if digit > 9:
-            digit = digit - 9
-
-        sum = sum + digit
-
-    return ( (sum % 10) == 0 )
-
 def cc_gen(bin):
 
     cc = ""
+
+    if len(bin) != 16:
+
+        while len(bin) != 16:
+            bin += 'x'
+
+    else:
+        pass
 
     if len(bin) == 16:
 
@@ -46,49 +36,75 @@ def cc_gen(bin):
             check_cc = cc
             check_cc += str(i)
 
-            if CHECKCARD(check_cc):
+            if ValidCC(check_cc):
                 cc = check_cc
                 break
             else:
                 check_cc = cc
 
-    elif len(bin) != 16:
-
-        while len(bin) != 16:
-            bin += 'x'
-
-        cc = ""
-
-        if len(bin) == 16:
-
-            for x in range(15):
-
-                if bin[x] in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
-                    cc += bin[x]
-                    continue
-
-                elif bin[x] in ('x'):
-                    cc += str(randint(0,9))
-
-                else:
-                    print(bad(f"Invalid Format Bin: {bin}"))
-                    sys.exit()
-
-
-            for i in range(10):
-                check_cc = cc
-                check_cc += str(i)
-
-                if CHECKCARD(check_cc):
-                    cc = check_cc
-                    break
-                else:
-                    check_cc = cc
-
     else:
         print(bad(f"Invalid Format BIN: {bin}"))
 
     return(cc)
+
+def ValidCC(card_number):
+    sum = 0
+    num_digits = len(card_number)
+    oddeven = num_digits & 1
+
+    for count in range(0, num_digits):
+        digit = int(card_number[count])
+
+        if not (( count & 1 ) ^ oddeven ):
+            digit = digit * 2
+        if digit > 9:
+            digit = digit - 9
+
+        sum = sum + digit
+
+    return ( (sum % 10) == 0 )
+
+
+def dategen(month, year):
+
+    if month == 'rnd' and year == 'rnd':
+        now = datetime.datetime.now()
+        month = int(randint(1, 12))
+        current_year = str(now.year)
+        year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+
+        if month < 10:
+            month = '0'+str(month)
+        
+        else:
+            month = str(month)
+
+        date = month + "|" + year
+        return date
+
+    elif month == 'rnd' and year != 'rnd':
+        now = datetime.datetime.now()
+        month = int(randint(1,12))
+
+        if month < 10:
+            month = '0'+str(month)
+        
+        else:
+            month = str(month)
+
+        date = month + "|" + year
+        return date
+
+    elif month != 'rnd' and year == 'rnd':
+        now = datetime.datetime.now()
+        current_year = str(now.year)
+        year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
+        date = month + "|" + year
+        return date
+
+    elif month != 'rnd' and year != 'rnd':
+        date = month + "|" + year
+        return date
 
 def ccvgen(cvv):
         
@@ -108,35 +124,6 @@ def ccvgen(cvv):
 
     else:
         return(cvv)
-
-def dategen(month, year):
-
-    if month == 'rnd' and year == 'rnd':
-        now = datetime.datetime.now()
-        month = str(randint(1, 12))
-        current_year = str(now.year)
-        year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
-        date = month + "|" + year
-        return date
-
-    elif month == 'rnd' and year != 'rnd':
-        now = datetime.datetime.now()
-        month = str(randint(1,12))
-        date = month + "|" + year
-        return date
-
-    elif month != 'rnd' and year == 'rnd':
-        now = datetime.datetime.now()
-        current_year = str(now.year)
-        year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
-        date = month + "|" + year
-        return date
-
-    elif month != 'rnd' and year != 'rnd':
-        date = month + "|" + year
-        return date
-
-
 
 def clean():
 
